170cd5b686565b2b5a4182b0ec8d964509689252
Austin Finn <awesomefinn43@gmail.com>
2016-01-19 09:49:38 +0000

    create route and view file for article

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-    root to: 'page#home'
-    get 'about' => 'page#about'
+  root to: 'page#home'
+  get 'about' => 'page#about'
 
+
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
