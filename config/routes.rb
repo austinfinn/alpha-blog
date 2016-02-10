ad383b7768d2d918e04ae741b540a42650b0ca73
Austin Finn <awesomefinn43@gmail.com>
2016-02-10 10:13:49 +0000

    add user actions

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   resources :articles
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   
