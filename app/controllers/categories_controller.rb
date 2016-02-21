0dffe14c73cf52c5236c5441ddcd4e654f629cef
Austin Finn <awesomefinn43@gmail.com>
2016-02-21 00:28:19 +0000

    edit category names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-  before_action :require_admin_user, only: [:create, :new]
+  before_action :require_admin_user, only: [:create, :new, :edit]
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -25,6 +25,21 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+end
   
   private
   def category_params
